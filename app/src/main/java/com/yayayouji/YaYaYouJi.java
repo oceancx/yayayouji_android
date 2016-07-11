171b51025f11fc95f849e16df88fb4602f585295
oceancx <oceancx@gmail.com>
2016-07-11 10:28:07 +0800

    删除shareSDK依赖

--- app/src/main/java/com/yayayouji/YaYaYouJi.java
+++ app/src/main/java/com/yayayouji/YaYaYouJi.java
@@ -21,9 +21,6 @@ import com.yayayouji.util.DebugLog;
 import java.io.IOException;
 import java.net.URLEncoder;
 
-import cn.sharesdk.framework.ShareSDK;
-import cn.sharesdk.onekeyshare.OnekeyShare;
-
 
 /**
  * Created by oceancx on 15/10/24.
@@ -42,7 +39,6 @@ public class YaYaYouJi extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        ShareSDK.initSDK(this);
 
         setContentView(R.layout.yayayouji);
 
@@ -63,7 +59,6 @@ public class YaYaYouJi extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                showShare();
             }
         });
 
@@ -119,33 +114,6 @@ public class YaYaYouJi extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    private void showShare() {
-        ShareSDK.initSDK(this);
-        OnekeyShare oks = new OnekeyShare();
-        //关闭sso授权
-        oks.disableSSOWhenAuthorize();
-
-        // title标题：微信、QQ（新浪微博不需要标题）
-        oks.setTitle("我是分享标题");  //最多30个字符
-
-        // text是分享文本：所有平台都需要这个字段
-        oks.setText("我是分享文本，啦啦啦~http://uestcbmi.com/");  //最多40个字符
-
-        // imagePath是图片的本地路径：除Linked-In以外的平台都支持此参数
-        //oks.setImagePath(Environment.getExternalStorageDirectory() + "/meinv.jpg");//确保SDcard下面存在此张图片
-
-        //网络图片的url：所有平台
-        oks.setImageUrl("http://7sby7r.com1.z0.glb.clouddn.com/CYSJ_02.jpg");//网络图片rul
-
-        // url：仅在微信（包括好友和朋友圈）中使用
-        oks.setUrl("http://sharesdk.cn");   //网友点进链接后，可以看到分享的详情
-
-        // Url：仅在QQ空间使用
-        oks.setTitleUrl("http://www.baidu.com");  //网友点进链接后，可以看到分享的详情
-
-        // 启动分享GUI
-        oks.show(this);
-    }
 
     private class NetTask extends AsyncTask<String, Void, String> {
         @Override
